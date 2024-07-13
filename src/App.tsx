dd72c649991fb23d26ea3976d526310fbdfc8e5c
Tomasz Laboj <tomasz.laboj@ocado.com>
2024-07-13 20:53:10 +0100

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import MathQuiz from "./components/math-quiz/MathQuiz";
-
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import CapitalsQuiz from "./components/geography-quiz/CapitalsQuiz";
+import RootLayout from "./Root";
 function App() {
-  return <MathQuiz />;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <RootLayout />,
+      children: [
+        {
+          path: "math-quiz",
+          element: <MathQuiz />,
+        },
+        {
+          path: "capitals-quiz",
+          element: <CapitalsQuiz />,
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/components/geography-quiz/CapitalsQuiz.tsx
@@ -0,0 +1,5 @@
+function CapitalsQuiz() {
+  return <h1>Capitals Quiz</h1>;
+}
+
+export default CapitalsQuiz;
