a22425606c7551cfb53785e10d0964390368155c
TomaszLaboj <tomasz.laboj@gmail.com>
2024-11-10 22:49:51 +0000

    add message to user to log in and then show points

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import CapitalsQuiz from "./components/geography-quiz/CapitalsQuiz";
 import RootLayout from "./Root";
 import Title from "./components/home/Title";
+import UserStatistics from "./components/home/UserStatistics";
 
 function App() {
   const [userLoggedIn, setUserLoggedIn] = useState<boolean>(false);
@@ -28,6 +29,11 @@ function App() {
     <>
       <Title loggedIn={userLoggedIn} logInOut={setUserLoggedIn} />
       <RouterProvider router={router} />
+      {userLoggedIn ? (
+        <UserStatistics userPoints={100000000000} />
+      ) : (
+        "Please log in to save progress"
+      )}
     </>
   );
 }
